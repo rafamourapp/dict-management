b6714d643b7ec362eb6ff6a6262d5efa468d8caa
Lucas Alves Da Silva <luca.silva@PPM-SPO-9042.local>
2020-08-06 17:14:49 -0300

    create querys

--- src/main/java/com/pp/dictmanagement/repository/KeyRepository.java
+++ src/main/java/com/pp/dictmanagement/repository/KeyRepository.java
@@ -2,6 +2,7 @@ package com.pp.dictmanagement.repository;
 
 import com.pp.dictmanagement.entity.Key;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,11 @@ import java.util.UUID;
 public interface KeyRepository extends JpaRepository<Key, UUID> {
 
     List<Key> findByUserId(UUID id);
+
+    @Query("SELECT k FROM Key k WHERE k.userId = 1? and (k.status like ACTIVE or CLAIMED)")
+    List<Key> findByUser(UUID id);
+
+    @Query("SELECT k FROM Key k WHERE k.value = 1? and (k.status like ACTIVE or CLAIMED)")
+    Key findByKeyValue(String keyValue);
 
 }
