555cf49e31f1f2b35f4128881e094c85536dcc42
Lucas Alves Da Silva <luca.silva@PPM-SPO-9042.local>
2020-08-07 11:57:50 -0300

    fix repository dialetc and jpa queries

--- src/main/java/com/pp/dictmanagement/repository/KeyRepository.java
+++ src/main/java/com/pp/dictmanagement/repository/KeyRepository.java
@@ -3,21 +3,22 @@ package com.pp.dictmanagement.repository;
 import com.pp.dictmanagement.entity.Key;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+nn
 @Repository
 public interface KeyRepository extends JpaRepository<Key, UUID> {
 
     List<Key> findAllByUserId(UUID id);
 
-    @Query("SELECT k FROM Key k WHERE k.userId = 1? and (k.status like ACTIVE or CLAIMED)")
-    List<Key> findAllActivatedByUser(UUID id);
+    @Query("SELECT k FROM Key k WHERE k.userId = ?1 and (k.status = 'ACTIVE' or k.status = 'CLAIMED')")
+    List<Key> findAllActivatedByUser(String id);
 
-    @Query("SELECT k FROM Key k WHERE k.value = 1? and (k.status like ACTIVE or CLAIMED)")
-    Optional<Key> findByKeyValue(String keyValue);
+    @Query("SELECT k FROM Key k WHERE k.keyValue = :value and (k.status = 'ACTIVE' or k.status = 'CLAIMED')")
+    Optional<Key> findByKeyValue(@Param("value") String keyValue);
 
 }
